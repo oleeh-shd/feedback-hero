70440865934bc4ce1f95eaf5f9fa6c84d4f6e38e
Serhii Popov <serhiipopov@MacBook-Pro-Serhii.local>
2022-04-02 11:48:20 +0300

    fixed mobile version 360px

--- src/components/HeaderBurger/HeaderBurger.js
+++ src/components/HeaderBurger/HeaderBurger.js
@@ -9,16 +9,18 @@ export const HeaderBurger = () => {
 
     return (
         <div className={styles.container}>
-            <div className={openBurger ? styles.burgerButtonActive : styles.burgerButton}
-                onClick={handleClick}>
+            <div className={openBurger ? styles.burgerActive : styles.burger}
+                 onClick={handleClick}>
                 <span className={styles.line}/>
             </div>
-            <div className={openBurger ? styles.dropDownWrapperActive : styles.dropDownWrapper}>
-                <Link to='/'>Features</Link>
-                <Link to='/'>Pricing</Link>
-                <Link to='/'>Blog</Link>
-                <div className={styles.wrapperBtn}>
-                    <Button btnClass="btnHeaderBurger">Register</Button>
+            <div className={styles.menuList}>
+                <div className={openBurger ? styles.active : styles.menuList}>
+                    <Link to='/'>Features</Link>
+                    <Link to='/'>Pricing</Link>
+                    <Link to='/'>Blog</Link>
+                    <div className={styles.wrapperBtn}>
+                        <Button btnClass="btnHeaderBurger">Register</Button>
+                    </div>
                 </div>
             </div>
         </div>
